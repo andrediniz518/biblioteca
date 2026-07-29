247f0a89f1b1ea4899c35cb43958634dc8e5e7ec
André Diniz <dinizandre518@gmail.com>
2026-07-29 01:16:10 -0300

    Adiciona cadastro de livros

--- livros/urls.py
+++ livros/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_livros, name='lista_livros'),
+    path('<int:id>/', views.detalhe_livro, name='detalhe_livro'),
+    path('cadastrar/', views.cadastrar_livro, name='cadastrar_livro'),
 ]
--- livros/views.py
+++ livros/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Livro
+from .forms import LivroForm
 
 
 # Create your views here.
 
 def lista_livros(request):
     livros = Livro.objects.all()
-
     contexto = {
         'livros': livros
     }
-
     return render(request, 'livros/lista_livros.html', contexto)
+
+
+def detalhe_livro(request, id):
+    livro = get_object_or_404(Livro, id=id)
+    contexto = {
+        'livro': livro
+
+    }
+    return render(request, 'livros/detalhe_livro.html', contexto)
+
+
+def cadastrar_livro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('lista_livros')
+    else:
+        form = LivroForm()
+    return render(request, 'livros/cadastrar_livro.html', {'form': form})
